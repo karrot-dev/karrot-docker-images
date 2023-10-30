e84a62859ce2d0f81add0649020af532783e2cc0
Nick Sellen <git@nicksellen.co.uk>
2023-10-30 23:19:50 +0000

    Build a backend-beta version from master branch

--- backend/build.sh
+++ backend/build.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-export VERSION="0.1.3"
-export VARIANT="backend"
+export VERSION="0.1.4"
+export VARIANT="backend-beta"
 export IMAGE="ghcr.io/karrot-dev/karrot-docker-images:${VERSION}-${VARIANT}"
 
 docker buildx build -t "${IMAGE}" .
